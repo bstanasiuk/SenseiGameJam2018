ad3de351a87f67386a83a45d00ae6bacb8432102
Kacper Szkodziński <kacper.szkodzinski@outlook.com>
2018-12-08 19:38:40 +0100

    Dodaje start screen

---
+++ Assets/GameStarter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStarter : MonoBehaviour {
+
+    public GameObject player0;
+    public GameObject player1;
+    public GameObject platformy;
+    public GameObject UICanvas;
+    public GameObject playmodeCanvas;
+    public GameObject startingScreen;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")||Input.GetButtonDown("Fire1_2")) {
+            player0.SetActive(true);
+            player1.SetActive(true);
+            platformy.SetActive(true);
+            UICanvas.SetActive(true);
+            playmodeCanvas.SetActive(true);
+            startingScreen.SetActive(false);
+        }
+	}
+}
