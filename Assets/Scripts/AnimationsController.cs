593fa95faa9ddfcc9c59ec2d7c8477b18ff4a01a
bstanasiuk <bartlomiej.stanasiuk@gmail.com>
2018-12-08 20:18:02 +0100

    Dodano podstawowe animacje

---
+++ Assets/Scripts/AnimationsController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class AttackCollisionController : MonoBehaviour
+{
+    [SerializeField] private bool _isBlack = false;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!_isBlack && other.gameObject.tag.Equals("BlackPlayer"))
+            UiController.Instance.AddPointForSecondPlayer();
+        else if (_isBlack && other.gameObject.tag.Equals("WhitePlayer"))
+            UiController.Instance.AddPointForFirstPlayer();
+    }
+}
---
+++ Assets/Scripts/AttackCollisionController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class AnimationsController : MonoBehaviour
+{
+    [SerializeField] private bool _isBlack = false;
+    private Animator _playerAnimator;
+
+    private Vector2 _lastPosition;
+
+    private void Awake()
+    {
+        _playerAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Vector2 currectPosition = transform.parent.position;
+        if (currectPosition != _lastPosition)
+        {
+            _playerAnimator.SetTrigger("WalkTrigger");
+        }
+        else
+        {
+            _playerAnimator.SetTrigger("IdleTrigger");
+        }
+        _lastPosition = currectPosition;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!_isBlack && other.gameObject.tag.Equals("BlackPlayer"))
+            UiController.Instance.AddPointForSecondPlayer();
+        else if (_isBlack && other.gameObject.tag.Equals("WhitePlayer"))
+            UiController.Instance.AddPointForFirstPlayer();
+    }
+
+    public void PlayAttackAnimation()
+    {
+        _playerAnimator.SetTrigger("AttackTrigger");
+    }
+}
