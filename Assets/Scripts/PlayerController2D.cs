c19c26ce9fbe852e97bef7a08ffbb4d8f3cb7c85
Kacper Szkodziński <kacper.szkodzinski@outlook.com>
2018-12-08 20:52:43 +0100

    Whoosh w kodzie

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -22,7 +22,7 @@ public class PlayerController2D : MonoBehaviour
 	private bool m_GroundedLastFrame = false;
 	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
 	private Rigidbody2D m_Rigidbody2D;
-	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+	public bool m_FacingRight = true;  // For determining which way the player is currently facing.
 	private Vector3 m_Velocity = Vector3.zero;
 	public int health;
 
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,30 +22,53 @@ public class PlayerMovement : MonoBehaviour
 	public Transform attackPos;
 	public LayerMask whoIsEnemy;
 	public int damage;
-	
+    public float dissapearWhoosh;
+
+    public GameObject lewyWhoosh;
+    public GameObject prawyWhoosh;
+
 	public AudioSource attackSound;
 	//public AudioSource stepsSound;
 	
 	private void Start()
 	{
+      
 		Physics2D.IgnoreLayerCollision(8, 8);
 		rb = GetComponent<Rigidbody2D>();
 	}
 
 	void Update ()
 	{
-		if (Player2 == false)
-		{
-			InputResponsePlayer1();
-			if(controller.m_Grounded == true)
-				rb.gravityScale = 3;
-		}
+        if (Player2 == false)
+        {
+            InputResponsePlayer1();
+            if (controller.m_Grounded == true)
+                rb.gravityScale = 3;
+
+            if (lewyWhoosh.activeSelf)
+            {
+                Invoke("WhooshDissapear", dissapearWhoosh);
+            }
+
+            if (prawyWhoosh.activeSelf)
+            {
+                Invoke("WhooshDissapear", dissapearWhoosh);
+            }
+        }
 		else
 		{
 			InputResponePlayer2();
 			if(controller.m_Grounded == true)
 				rb.gravityScale = 3;
-		}
+            if (lewyWhoosh.activeSelf)
+            {
+                Invoke("WhooshDissapear", dissapearWhoosh);
+            }
+            if (prawyWhoosh.activeSelf)
+            {
+                Invoke("WhooshDissapear", dissapearWhoosh);
+            }
+        }
 
 	}
 
@@ -62,6 +85,24 @@ public class PlayerMovement : MonoBehaviour
 		Gizmos.color = new Color(1, 0, 0, 1);	
 		Gizmos.DrawWireCube(attackPos.position, new Vector3(attackRangeX, attackRangeY, 1));
 	}
+
+    void WhooshDissapear()
+    {
+        lewyWhoosh.SetActive(false);
+        prawyWhoosh.SetActive(false);
+    }
+
+    void WhooshDisplay()
+    {
+        if (controller.m_FacingRight)
+        {
+            prawyWhoosh.SetActive(true);
+}
+        else
+        {
+            lewyWhoosh.SetActive(false);
+        }
+    }
 
 	void InputResponsePlayer1()
 	{
@@ -83,6 +124,8 @@ public class PlayerMovement : MonoBehaviour
 		{
 			if (Input.GetButtonDown("Fire1"))
 			{
+                WhooshDisplay();
+
 			    _animationsController.PlayAttackAnimation();
                 attackSound.Play();
 				Collider2D[] enemyToDamage = Physics2D.OverlapBoxAll(attackPos.position,
@@ -91,14 +134,17 @@ public class PlayerMovement : MonoBehaviour
 				{
 					enemyToDamage[i].GetComponent<PlayerController2D>().TakeDamage(damage);
 				}
-				timeBtwAttack = startTimeBtwAttack;	
+				timeBtwAttack = startTimeBtwAttack;
+
 			}
 
 			if (controller.m_Grounded == false)
 			{
 				if (Input.GetButtonDown("Fire2"))
 				{
-				    _animationsController.PlayAttackAnimation();
+                    WhooshDisplay();
+
+                    _animationsController.PlayAttackAnimation();
                     rb.gravityScale = 1000;
 					attackSound.Play();
 					Collider2D[] enemyToDamage = Physics2D.OverlapBoxAll(attackPos.position,
@@ -107,13 +153,14 @@ public class PlayerMovement : MonoBehaviour
 					{
 						enemyToDamage[i].GetComponent<PlayerController2D>().TakeDamage(damage);
 					}
-				}
+
+                }
 			}
 
 		}
 		else
 		{
-			timeBtwAttack -= Time.deltaTime;
+            timeBtwAttack -= Time.deltaTime;
 		}
 	}
 
@@ -137,7 +184,9 @@ public class PlayerMovement : MonoBehaviour
 		{
 			if (Input.GetButtonDown("Fire1_2"))
 			{
-				attackSound.Play();
+                WhooshDisplay();
+
+                attackSound.Play();
 				Collider2D[] enemyToDamage = Physics2D.OverlapBoxAll(attackPos.position,
 					new Vector2(attackRangeX, attackRangeY), 0, whoIsEnemy);
 				for (int i = 0; i < enemyToDamage.Length; i++)
@@ -146,11 +195,14 @@ public class PlayerMovement : MonoBehaviour
 				}
 				timeBtwAttack = startTimeBtwAttack;
                 _animationsController.PlayAttackAnimation();
-			}
+
+            }
 			if (controller.m_Grounded == false)
 			{
 				if (Input.GetButtonDown("Fire2_2"))
 				{
+                    WhooshDisplay();
+
                     _animationsController.PlayAttackAnimation();
                     rb.gravityScale = 1000;
 					attackSound.Play();
@@ -160,7 +212,8 @@ public class PlayerMovement : MonoBehaviour
 					{
 						enemyToDamage[i].GetComponent<PlayerController2D>().TakeDamage(damage);
 					}
-				}
+                    
+                }
 			}
 		}
 		else
